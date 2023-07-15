cce496e829ba95c333ecfeae2594bbd2d57882c4
Guillermo Tala <guille86598@gmail.com>
2023-07-15 14:19:29 +0200

    sepolia

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,16 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("hardhat-deploy");
+require("@nomicfoundation/hardhat-verify");
 
-/** @type import('hardhat/config').HardhatUserConfig */
+const PRIVATE_KEY = "";
+const INFURA_API_KEY = "";
+
 module.exports = {
   solidity: "0.8.19",
+  networks: {
+    sepolia: {
+      url: `https://sepolia.infura.io/v3/${INFURA_API_KEY}`,
+      accounts: [PRIVATE_KEY],
+    },
+  },
 };
