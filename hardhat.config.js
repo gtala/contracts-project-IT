b6a69ab8deb3b0ed4a2555b234a7699ddb3c0a7c
Guillermo Tala <guille86598@gmail.com>
2023-07-15 14:21:32 +0200

    verify

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,9 +1,8 @@
 require("@nomicfoundation/hardhat-toolbox");
-require("hardhat-deploy");
-require("@nomicfoundation/hardhat-verify");
 
-const PRIVATE_KEY = "";
-const INFURA_API_KEY = "";
+const PRIVATE_KEY =
+  "a237d23838b297feebfe5e615c1772f3a3bbb650f990802707083520134cbc62";
+const INFURA_API_KEY = "3917b72abea64f19b1161bb4108f10b6";
 
 module.exports = {
   solidity: "0.8.19",
